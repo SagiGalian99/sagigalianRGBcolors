b9ff547a7df1b61ba07f3f1c434805658ec1e8a3
SagiGalian99 <89161848+SagiGalian99@users.noreply.github.com>
2021-12-11 18:13:02 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,39 @@
+function mode1(){
+    document.getElementById("middleDivMode1").style.visibility = "visible";
+    document.getElementById("middleDivMode2").style.visibility = "hidden";
+    document.getElementById("middleDivDefault").style.visibility = "hidden";
+}
+function mode2(){
+    document.getElementById("middleDivMode1").style.visibility = "hidden";
+    document.getElementById("middleDivMode2").style.visibility = "visible";
+    document.getElementById("middleDivDefault").style.visibility = "hidden";
+}
+function mode3(){
+    document.getElementById("middleDivMode1").style.visibility = "hidden";
+    document.getElementById("middleDivMode2").style.visibility = "hidden";
+    document.getElementById("middleDivDefault").style.visibility = "visible";
+}
+
+function seeColor(){
+    var red = document.getElementById("red").value;
+    var green = document.getElementById("green").value;
+    var blue = document.getElementById("blue").value;
+
+    document.getElementById("colorSee").style.backgroundColor = "rgb(" + red.toString() + "," + green.toString() + "," + blue.toString() + ")";
+}
+function findRgbColor(){
+    const colors = ["red","green","blue"];
+    const rgb = ["(255, 0, 0)","(0, 255, 0)","(0, 0, 255)"];
+
+    var color = document.getElementById("colorName").value;
+    var findColor = false;
+    for (var i = 0; i < colors.length; i++){
+        if (color == colors[i]){
+            document.getElementById("rgbName").innerHTML = rgb[i];
+            findColor = true;
+        }
+    }
+    if (findColor == false){
+        document.getElementById("rgbName").innerHTML = "Cant find this color"
+    }
+}
